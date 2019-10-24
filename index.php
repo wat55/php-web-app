b929efdda8e1cd00157f9d697257535790dbb846
wat55 <monta.kazocina@gmail.com>
2019-10-24 08:34:27 +0000

    jsjgbkwsg

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
+<html>
+<head>
+    <title>AmAnDa</title>
+</head>
+<body>
+<table border="1">
+<tr>
+<td>
 <?php
 
-echo "Hello World abuharis testing"
+echo "Hello Amanda"
 
 ?>
+</td>
+<img src="https://imgflip.com/s/meme/Mocking-Spongebob.jpg" alt="lll" height="300" width="400">
+</tr>
+</body>
+</html>
